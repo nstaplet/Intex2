273e6bde6a6f905efe08c4b25e453cce90b19d15
nstaplet <nstaplet7@gmail.com>
2021-04-07 18:40:18 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Intex.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
