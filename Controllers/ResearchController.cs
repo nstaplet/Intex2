97f7054a5e731faa747c130c6a232d3781caab09
Ethan Hollis <e.hollis32@gmail.com>
2021-04-08 13:29:42 -0600

    add research controller

---
+++ Controllers/ResearchController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intex.Controllers
+{
+
+    [Authorize(Roles = "Researcher,Admin")]
+    public class ResearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
