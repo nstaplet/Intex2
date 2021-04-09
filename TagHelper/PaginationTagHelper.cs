9d49bcbb6e31cc35b5b498d1eb9bb083d990338d
petergardiner25 <50341968+petergardiner25@users.noreply.github.com>
2021-04-09 12:48:44 -0600

    comment tag helpet

--- TagHelper/PaginationTagHelper.cs
+++ TagHelper/PaginationTagHelper.cs
@@ -1,50 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.Routing;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using Microsoft.AspNetCore.Razor.TagHelpers;
-using Intex.Models.ViewModels;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.Rendering;
+//using Microsoft.AspNetCore.Mvc.Routing;
+//using Microsoft.AspNetCore.Mvc.ViewFeatures;
+//using Microsoft.AspNetCore.Razor.TagHelpers;
+//using Intex.Models.ViewModels;
 
-namespace Intex.TagHelper
-{
-    [HtmlTargetElement("div", Attributes = "page-info")]
-    public class PaginationTagHelper : TagHelper
-    {
-        private IUrlHelperFactory urlInfo;
-        public PaginationTagHelper(IUrlHelperFactory uhf)
-        {
-            urlInfo = uhf;
-        }
+//namespace Intex.TagHelper
+//{
+//    [HtmlTargetElement("div", Attributes = "page-info")]
+//    public class PaginationTagHelper : TagHelper
+//    {
+//        private IUrlHelperFactory urlInfo;
+//        public PaginationTagHelper(IUrlHelperFactory uhf)
+//        {
+//            urlInfo = uhf;
+//        }
 
-        public PageNumbering PageInfo { get; set; }
+//        public PageNumbering PageInfo { get; set; }
 
-        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
-        public Dictionary<string, object> KeyValuePairs { get; set; } = new Dictionary<string, object>();
-        [HtmlAttributeNotBound]
-        [ViewContext]
-        public ViewContext viewContext { get; set; }
+//        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+//        public Dictionary<string, object> KeyValuePairs { get; set; } = new Dictionary<string, object>();
+//        [HtmlAttributeNotBound]
+//        [ViewContext]
+//        public ViewContext viewContext { get; set; }
 
-        public override void Process(TagHelperContext context, TagHelperOutput output)
-        {
-            TagBuilder finishedTag = new TagBuilder("div");
+//        public override void Process(TagHelperContext context, TagHelperOutput output)
+//        {
+//            TagBuilder finishedTag = new TagBuilder("div");
 
-            IUrlHelper urlHelp = urlInfo.GetUrlHelper(viewContext);
+//            IUrlHelper urlHelp = urlInfo.GetUrlHelper(viewContext);
 
-            for (int i = 1; i <= PageInfo.NumPages; i++)
-            {
-                TagBuilder individualTag = new TagBuilder("a");
+//            for (int i = 1; i <= PageInfo.NumPages; i++)
+//            {
+//                TagBuilder individualTag = new TagBuilder("a");
 
-                KeyValuePairs["pageNum"] = i;
-                individualTag.Attributes["href"] = urlHelp.Action("Index", KeyValuePairs);
-                individualTag.InnerHtml.Append(i.ToString());
+//                KeyValuePairs["pageNum"] = i;
+//                individualTag.Attributes["href"] = urlHelp.Action("Index", KeyValuePairs);
+//                individualTag.InnerHtml.Append(i.ToString());
 
-                finishedTag.InnerHtml.AppendHtml(individualTag);
-            }
-            output.Content.AppendHtml(finishedTag.InnerHtml);
-        }
-    }
-}
+//                finishedTag.InnerHtml.AppendHtml(individualTag);
+//            }
+//            output.Content.AppendHtml(finishedTag.InnerHtml);
+//        }
+//    }
+//}
