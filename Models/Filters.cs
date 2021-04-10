978358c108209b6c1cdf2f46ce265a33dc5bc5d7
petergardiner25 <50341968+petergardiner25@users.noreply.github.com>
2021-04-10 11:12:29 -0600

    delete this model if you want

---
+++ Models/Filters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intex.Models
+{
+    public class Filters
+    {
+
+
+        public Filters(string filterstring)
+        {
+            FilterString = filterstring ?? "all";
+            string[] filters = FilterString.Split('-');
+            HairColor = filters[0];
+            //BurialDirection = filters[1];
+
+
+
+        }
+
+        public string FilterString { get; }
+        public string HairColor { get; }
+       // public string BurialDirection { get; }
+
+
+
+        public bool HasHairColor => HairColor.ToLower() != "all";
+      //  public bool HasBurialDirection => BurialDirection.ToLower() != "all";
+
+    }
+}
